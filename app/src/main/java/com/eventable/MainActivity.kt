f8cc8177ab0ca8f682d1c49cf27728bd2fe5d323
Christopher Augustin <christopher.augustin@st.oth-regensburg.de>
2022-05-29 16:38:29 +0200

    init testfiles to connect with the database

--- app/src/main/java/com/eventable/MainActivity.kt
+++ app/src/main/java/com/eventable/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.eventable
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,30 @@ class MainActivity : AppCompatActivity() {
         Log.e("TAG", "test msg")
 
         Log.e("TAG", "Ein anderer Test")
+
+        val db = Firebase.firestore
+
+        //Create a new User
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Meier",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+
+
+
+
 
 
     }
