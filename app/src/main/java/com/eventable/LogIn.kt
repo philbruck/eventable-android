4fe0538940200cb2494a90fb6679dbb34346c95e
Christopher Augustin <christopher.augustin@st.oth-regensburg.de>
2022-05-30 21:56:01 +0200

    data can be added and read from database

--- app/src/main/java/com/eventable/LogIn.kt
+++ app/src/main/java/com/eventable/LogIn.kt
@@ -7,5 +7,8 @@ class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+
+
     }
 }
--- app/src/main/java/com/eventable/MainActivity.kt
+++ app/src/main/java/com/eventable/MainActivity.kt
@@ -18,24 +18,48 @@ class MainActivity : AppCompatActivity() {
 
         val db = Firebase.firestore
 
-        //Create a new User
-        val user = hashMapOf(
-            "first" to "Ada",
-            "last" to "Meier",
-            "born" to 1815
-        )
 
-        // Add a new document with a generated ID
+     //--> Neuer User wird unter der Sammlung users mit automatisch generierte ID angelegt
+//// Create a new user with a first and last name
+//        val user = hashMapOf(
+//            "first" to "Christopher",
+//            "last" to "Augustin",
+//            "born" to 1997
+//        )
+//// Add a new document with a generated ID
+//        db.collection("users")
+//            .add(user)
+//            .addOnSuccessListener { documentReference ->
+//                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.w(TAG, "Error adding document", e)
+//            }
+
+//   --> Daten auslesen
+
         db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
             }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
             }
 
+        val cities = db.collection("cities")
 
+        val data1 = hashMapOf(
+            "name" to "San Francisco",
+            "state" to "CA",
+            "country" to "USA",
+            "capital" to false,
+            "population" to 860000,
+            "regions" to listOf("west_coast", "norcal")
+        )
+        cities.document("name").set(data1)
 
 
 
